ba3c4e16fd7d5cd86447e273035a8f37fac6b3ed
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-12-30 23:13:35 +0000

    add press b to clear strip

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     strip.showRainbow(1, 360)
 })
+input.onButtonPressed(Button.B, function () {
+    strip.clear()
+    strip.show()
+})
 let strip: neopixel.Strip = null
-basic.showIcon(IconNames.Tortoise)
+basic.showIcon(IconNames.Yes)
 strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
-strip.setBrightness(32)
+strip.setBrightness(8)
 strip.clear()
 strip.show()
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     strip.rotate(1)
     strip.show()
